0cc7ae9f522916ddd5822d0a28bc3c0683c96845
julipache <96998878+julipache@users.noreply.github.com>
2024-05-21 19:37:03 +0200

    v

--- tensor/app/pruebamsiva.py
+++ tensor/app/pruebamsiva.py
@@ -44,8 +44,6 @@ def predict_directory_images(directory_path, model_path, confidence_threshold=60
             predicted_class_index = np.argmax(predictions, axis=1)
             predicted_class_name = class_labels[predicted_class_index[0]]
             confidence = np.max(predictions) * 100
-            print(f"File: {filename} - Predicted: {predicted_class_name} ({confidence:.2f}%), Actual: {actual_class_name}")
-
 
             if confidence < confidence_threshold:
                 print(f"File: {filename} - Prediction confidence ({confidence:.2f}%) below threshold, no prediction made.")
@@ -56,6 +54,7 @@ def predict_directory_images(directory_path, model_path, confidence_threshold=60
             os.makedirs(target_folder, exist_ok=True)
             shutil.move(img_path, os.path.join(target_folder, filename))
 
+            print(f"File: {filename} - Predicted: {predicted_class_name} ({confidence:.2f}%), Actual: {actual_class_name}")
 
             if actual_class_name in class_labels.values():
                 total_predictions[actual_class_name] += 1
