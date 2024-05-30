42949cc50df3e772806a1d808572a3d11059cbca
julipache <96998878+julipache@users.noreply.github.com>
2024-05-30 20:51:48 +0200

    v

--- tensor/app/run_cron.sh
+++ tensor/app/run_cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Crear un archivo crontab
+echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" > /etc/cron.d/my_cron
 echo "*/10 * * * * /usr/bin/python /app/script.py >> /var/log/cron.log 2>&1" > /etc/cron.d/my_cron
 echo "*/30 * * * * /usr/bin/python /app/enviar_email.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/my_cron
 
