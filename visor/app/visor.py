d0723d553b84a5f0f6b3d2c36563d011feccdf7f
julipache <96998878+julipache@users.noreply.github.com>
2024-06-20 12:29:57 +0200

    v

--- visor/app/visor.py
+++ visor/app/visor.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, Response
+import subprocess
 import os
 
 app = Flask(__name__)
@@ -11,29 +12,57 @@ html_template = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RTSP Cameras</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jsmpeg/0.2/jsmpeg.min.js"></script>
 </head>
 <body>
     <h1>RTSP Cameras</h1>
     <div>
-        <video width="640" height="480" controls>
-            <source src="{{ rtsp_url_1 }}" type="application/x-rtsp">
-            Your browser does not support the RTSP stream.
-        </video>
-        <video width="640" height="480" controls>
-            <source src="{{ rtsp_url_2 }}" type="application/x-rtsp">
-            Your browser does not support the RTSP stream.
-        </video>
+        <canvas id="videoCanvas1" width="640" height="480"></canvas>
+        <canvas id="videoCanvas2" width="640" height="480"></canvas>
         <!-- Puedes añadir más streams aquí -->
     </div>
+    <script>
+        const player1 = new JSMpeg.Player('ws://' + window.location.host + '/video_feed_1', {
+            canvas: document.getElementById('videoCanvas1')
+        });
+        const player2 = new JSMpeg.Player('ws://' + window.location.host + '/video_feed_2', {
+            canvas: document.getElementById('videoCanvas2')
+        });
+    </script>
 </body>
 </html>
 """
 
 @app.route('/')
 def index():
+    return render_template_string(html_template)
+
+def generate_frames(rtsp_url):
+    command = [
+        'ffmpeg',
+        '-i', rtsp_url,
+        '-f', 'mpegts',
+        '-codec:v', 'mpeg1video',
+        '-bf', '0',
+        '-q', '0',
+        '-'
+    ]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+        frame = process.stdout.read(1024)
+        if not frame:
+            break
+        yield frame
+
+@app.route('/video_feed_1')
+def video_feed_1():
     rtsp_url_1 = os.getenv('RTSP_URL_1')
+    return Response(generate_frames(rtsp_url_1), mimetype='video/mp2t')
+
+@app.route('/video_feed_2')
+def video_feed_2():
     rtsp_url_2 = os.getenv('RTSP_URL_2')
-    return render_template_string(html_template, rtsp_url_1=rtsp_url_1, rtsp_url_2=rtsp_url_2)
+    return Response(generate_frames(rtsp_url_2), mimetype='video/mp2t')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8099)
